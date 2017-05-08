51c013e922cb5848e7526c76418445645db123f7
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-05-08 00:34:17 -0300

    Fix routing when user is not an editor

--- app/config/PrivateRoute.js
+++ app/config/PrivateRoute.js
@@ -3,12 +3,14 @@ import { Loading } from 'components'
 import { Route } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  return <Route {...rest} render={props => (
-    rest.allow ? (
-      <Component {...props} />
-    ) : (
-      <Loading />
-    )
-  )}/>
+  return <Route {...rest} render={props => {
+    if (rest.allow) {
+      return <Component {...props} />
+    } else if (rest.isLoading) {
+      return <Loading />
+    } else {
+      return <Route component={() => <div>Pagina nao encontrada</div>} />
+    }
+  }}/>
 }
 export default PrivateRoute
