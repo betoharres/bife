5ed444d285d923e5376cd0d873b908f80dc57fb3
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-07-10 20:33:23 -0300

    Compile modules written in ES6

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -84,7 +84,6 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-      
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -158,7 +157,7 @@ module.exports = {
       // Process JS with Babel.
       {
         test: /\.(js|jsx)$/,
-        include: paths.appSrc,
+        include: [paths.appSrc, paths.appNodeModules],
         loader: require.resolve('babel-loader'),
         
       },
