68c04aece9e0a6da8a93996d5a71cd435da0dc23
Alberto Harres Rocha <albertohrocha@gmail.com>
2017-09-05 22:16:43 -0300

    Remove unused container

--- src/containers/Home/HomeContainer.js
+++ src/containers/Home/HomeContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Home } from '../../components'
-import { NewPostContainer } from '../../containers'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
